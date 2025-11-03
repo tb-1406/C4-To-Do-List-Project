024d575e7f23c088c0642d7fbb75889ca76e3922
MarvLeeds <zcwc0557@leeds.ac.uk>
2025-11-03 12:34:04 +0000

    prototype_1

--- code/mainUI.py
+++ code/mainUI.py
@@ -1,1 +1,16 @@
 # Team work on this together
+tasks = 0
+
+#function called to check number of tasks
+
+
+print("")
+print("")
+if tasks == 0:
+    print("No tasks currently. To add tasks, type 'add [task name]'")
+
+print("")
+print("-----------------------------------------------------------------------------")
+print("")
+
+cmdInput = input("->  ")
