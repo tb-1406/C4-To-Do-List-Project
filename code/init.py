b49dadd3073c6b19978572e1d8443a33465ac061
MarvLeeds <zcwc0557@leeds.ac.uk>
2025-11-03 17:39:46 +0000

    idek icl

---
+++ code/init.py
@@ -0,0 +1,6 @@
+from todoFunctions.add import *
+from todoFunctions.delete import *
+from todoFunctions.write import *
+from todoFunctions.read import *
+from todoFunctions.mark import *
+from todoFunctions.modify import *
--- code/mainUI.py
+++ code/mainUI.py
@@ -1,15 +1,19 @@
 # Team work on this together
-
+import os
+from init import *
 
 tasks = []
 #function called to check number of tasks
 
 while True:
+    os.system('cls')
+
     print("-----------------------------------------------------------------------------")
     print("")
     if len(tasks) == 0:
         print("No tasks currently. To add tasks, type 'add [task name]'")
     else:
+        print("To add tasks, type 'add [task name]'")
         for i, task in enumerate(tasks):
             print(f"[{i + 1}]", task)
 
