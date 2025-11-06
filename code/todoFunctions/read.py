4ea2b21ea2bb5d15b2724e88d6bf651a73ebccf5
TahmidAsraf <lgrq0742@leeds.ac.uk>
2025-11-06 17:19:29 +0000

    array indexes are put onto a text file called inst.txt

--- code/todoFunctions/read.py
+++ code/todoFunctions/read.py
@@ -1,2 +1,17 @@
 # Tiago & Endu
 #Navigator: Tiago 
+
+
+def read(task = [], *args ):
+    with open("inst","w") as f:
+        for item in task:
+            f.write(str(item) + "\n")
+    
+
+    with open("inst", "r") as f:
+            print(f.read())
+
+
+
+
+
